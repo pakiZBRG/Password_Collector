7e2151e2bd5ea118a4b3da5bcf2f46802ecaac45
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-04-20 14:49:54 +0200

    Added hero background and animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import './style.scss';
 import React from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import Home from './components/Home';
 
 function App() {
-  return (
-    <div>
-      Nikola
-    </div>
+  const location = useLocation();
+
+    return (
+      <AnimatePresence /*initial={false}*/ exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path='/' component={Home}/>
+        </Switch>
+      </AnimatePresence>
   );
 }
 
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+function Home() {
+    const easeOut = { duration: .8, ease: [.42, 0, .58, 1] };
+
+    return (
+        <div className='background'>
+            <motion.div
+                initial={{ scaleX: 0, transformOrigin: 'left'}}
+                animate={{ scaleX: 1}}
+                transition={easeOut}
+                className="background-red"
+            ></motion.div>
+            <motion.div
+                initial={{ scaleY: 0, transformOrigin: 'top'}}
+                animate={{ scaleY: 1}}
+                transition={{delay: 0.3, ...easeOut}}
+                className="background-green"
+            ></motion.div>
+            <motion.div
+                initial={{ scaleY: 0, transformOrigin: 'bottom'}}
+                animate={{ scaleY: 1}}
+                transition={{delay: 0.5, ...easeOut}}
+                className="background-blue"
+            ></motion.div>
+            <motion.div
+                initial={{ scaleX: 0, transformOrigin: 'left'}}
+                animate={{ scaleX: 1}}
+                transition={{delay: 0.8, ...easeOut}}
+                className="background-yellow"
+            ></motion.div>
+            <motion.div
+                initial={{ scaleX: 0, transformOrigin: 'right'}}
+                animate={{ scaleX: 1}}
+                transition={{delay: 1, ...easeOut}}
+                className="background-beige"
+            ></motion.div>
+        </div>
+    )
+}
+
+export default Home
