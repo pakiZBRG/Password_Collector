b0bca329d51ad543aa17b210861593107c24eb45
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-04-26 08:02:02 +0200

    Refactor register code / add responsiveness

---
+++ client/src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './RegisterForm.scss';
+
+function RegisterForm() {
+    const showPassword = () => {
+        const pass = document.querySelectorAll("#password");
+        pass.forEach(p => p.type === "password" ? p.type = "text" : p.type = "password");
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log('Form Submitted');
+    }
+
+    return (
+        <div className='register-form'>
+            <h1>Create An Account</h1>
+            <form onSubmit={() => handleSubmit(this)} className="form">
+                <div className='form-input'>
+                    <label htmlFor='email'>Email Address</label>
+                    <input
+                        type='email'
+                        name='email'
+                        placeholder='Enter your email'
+                        autoComplete='off'
+                    />
+                </div>
+                <div className='form-input'>
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        id='password'
+                        type='password'
+                        name='password'
+                        placeholder='Enter your password'
+                    />
+                </div>
+                <div className='form-input'>
+                    <label htmlFor='passwordRepeat'>Repeat Password</label>
+                    <input
+                        type="password"
+                        id='password'
+                        name='passwordRepeat'
+                        placeholder='Repeat your password'
+                    />
+                </div>
+                <div className='form-show'>
+                    <input type="checkbox" onChange={() => showPassword()}/>
+                    <p>Show password</p>
+                </div>
+                <input value='Sign up &rarr;' type='submit'className='form-button'/>
+                <p className='have-account'>Have an account? <Link to='/login'>Login</Link></p>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterForm
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,65 +1,28 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import RegisterForm from '../components/RegisterForm/RegisterForm';
 
 function Register() {
     const easeOut = { duration: 1, ease: [.42, 0, .58, 1] };
-
-    const showPassword = () => {
-        const pass = document.querySelectorAll("#password");
-        pass.forEach(p => p.type === "password" ? p.type = "text" : p.type = "password");
-    }
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        console.log('Form Submitted');
-    }
     
     return (
         <div className='background'>
             <motion.div
-                animate={{height: '100vh', width: '100vw', ease: 'easeInOut'}}
+                animate={{ height: '100vh', width: '100vw' }}
+                exit={{ height: '70vh', width: 0 }}
+                transition={{ ...easeOut}}
                 className="background-red"
             >
-                <div className='register'>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.8 }}
+                    transition={{ ...easeOut }}
+                    className='register'
+                >
                     <div className='register-image'></div>
-                    <div className='register-form'>
-                        <h1>Create An Account</h1>
-                        <form onSubmit={() => handleSubmit(this)} className="form">
-                            <div className='form-input'>
-                                <label htmlFor='email'>Email Address</label>
-                                <input
-                                    type='email'
-                                    name='email'
-                                    placeholder='Enter your email'
-                                    autoComplete='off'
-                                />
-                            </div>
-                            <div className='form-input'>
-                                <label htmlFor='password'>Password</label>
-                                <input
-                                    id='password'
-                                    type='password'
-                                    name='password'
-                                    placeholder='Enter your password'
-                                />
-                            </div>
-                            <div className='form-input'>
-                                <label htmlFor='passwordRepeat'>Repeat Password</label>
-                                <input
-                                    type="password"
-                                    id='password'
-                                    name='passwordRepeat'
-                                    placeholder='Repeat your password'
-                                />
-                            </div>
-                            <div className='form-input'>
-                                <input type="checkbox" onChange={() => showPassword()}/>
-                                <p>Show password</p>
-                            </div>
-                            <button type='submit'className='form-button'>Sign Up</button>
-                        </form>
-                    </div>
-                </div>
+                    <RegisterForm />
+                </motion.div>
             </motion.div>
         </div>
     )
