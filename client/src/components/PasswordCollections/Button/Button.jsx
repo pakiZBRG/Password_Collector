3598e187dd7956f82fb034628f6e40d0f29b811d
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-05-19 14:31:37 +0200

    Small changes in style

--- client/src/components/PasswordCollections/Button/Button.jsx
+++ client/src/components/PasswordCollections/Button/Button.jsx
@@ -2,9 +2,6 @@ import './Button.scss'
 
 export const Button = ({text}) => (
     <button className="fancy">
-        <span className="top-key"></span>
         <input type='submit' value={text}/>
-        <span className="bottom-key-1"></span>
-        <span className="bottom-key-2"></span>
     </button>
 )
--- client/src/components/PasswordCollections/Passwords/Passwords.jsx
+++ client/src/components/PasswordCollections/Passwords/Passwords.jsx
@@ -64,27 +64,27 @@ function Passwords({toggle, passwords, config}) {
         <>
             <ToastContainer/>
             {passwords.collection && 
-            <div className='password'>
-                <div className='password-info'>
-                    <h1>{passwords.collection.name}</h1>
-                    <a target='blanc' href={`https://${passwords.collection.website}`}>{passwords.collection.website}</a>
-                    <h3>{passwords.collection.category}</h3>
-                </div>
-                <div className='password-pass'>
-                    <table>
-                        <thead>
-                            <tr>
-                                <td>#</td>
-                                <td>Email</td>
-                                <td>Password</td>
-                                <td>Action</td>
-                            </tr>
-                        </thead>
-                        {!passwords.collection.passwords.length ? 
-                            <p className='password-nopass'>No Passwords</p>
-                                :
+                <div className='password'>
+                    <div className='password-info'>
+                        <h1>{passwords.collection.name}</h1>
+                        <a target='blanc' href={`https://${passwords.collection.website}`}>{passwords.collection.website}</a>
+                        <h3>{passwords.collection.category}</h3>
+                    </div>
+                    <div className='password-pass'>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>#</td>
+                                    <td>Email</td>
+                                    <td>Password</td>
+                                    <td>Action</td>
+                                </tr>
+                            </thead>
                             <tbody>
-                                {passwords.collection.passwords.map((pass, i) => 
+                            {!passwords.collection.passwords.length ? 
+                                <tr className='password-nopass'><td>No Passwords</td></tr>
+                                    :
+                                <>{passwords.collection.passwords.map((pass, i) => 
                                     <tr key={pass._id}>
                                         <td>{i+1}.</td>
                                         <td>{pass.email}</td>
@@ -102,13 +102,14 @@ function Passwords({toggle, passwords, config}) {
                                             <i onClick={removePassword} className="fa fa-remove icon"></i>
                                         </td>
                                     </tr>
-                                )}
+                                )}</>
+                            }
                             </tbody>
-                        }
-                    </table>
+                        </table>
+                    </div>
+                    <span className='password-close' onClick={toggle}>&times;</span>
                 </div>
-                <span className='password-close' onClick={toggle}>&times;</span>
-            </div>}
+            }
         </>
     )
 }
